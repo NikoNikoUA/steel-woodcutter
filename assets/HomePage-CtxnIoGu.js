d2d823b2ec02ada1a39e490dbbe6c798b27a459a
NikoNikoUA <NikoNikoUA@users.noreply.github.com>
2024-07-25 18:45:58 +0000

    deploy: 2132623cfa5a9cdae6cdec730f8dc6b581530acb

---
+++ assets/HomePage-CtxnIoGu.js
@@ -0,0 +1,1 @@
+import{j as e,L as s}from"./index-b6r3SzyV.js";const i=()=>e.jsxs(e.Fragment,{children:[e.jsx("div",{children:"HomePage"}),e.jsxs("ul",{children:[e.jsx("li",{children:e.jsx(s,{to:"/categories",children:"categories"})}),e.jsx("li",{children:e.jsx(s,{to:"/favorites",children:"Favorites"})})]})]});export{i as default};
---
+++ assets/NotFound-BlYY--vc.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-b6r3SzyV.js";const r=()=>o.jsx("div",{children:"NotFound"});export{r as default};
