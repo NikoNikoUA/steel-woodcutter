6e33dc7eb5e031ad2ff2b314235cbe90a1c2e36b
NikoNikoUA <NikoNikoUA@users.noreply.github.com>
2024-07-27 14:30:46 +0000

    deploy: fbec91265a93e48c299e4dd9bbb9eb962045a364

--- assets/Favorites-DG5fBuwv.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-DZ4AtYJn.js";const s=()=>r.jsx("div",{children:"Favorites"});export{s as default};
--- assets/NotFound-VDe_QLwm.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-DZ4AtYJn.js";const r=()=>o.jsx("div",{children:"NotFound"});export{r as default};
