fae14e72d5e7c74239e28e07123d9a25d56c1957
NikoNikoUA <NikoNikoUA@users.noreply.github.com>
2024-08-12 20:00:14 +0000

    deploy: a8e61455a3c4121be9c9ae76c4dbc57cf39e8f4d

--- assets/Favorites-Bvlab3Ls.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-BoGT7PfZ.js";const s=()=>r.jsx("div",{children:"Favorites"});export{s as default};
---
+++ assets/Favorites-DAZB4e7E.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-ICITsXol.js";const s=()=>r.jsx("div",{children:"Favorites"});export{s as default};
