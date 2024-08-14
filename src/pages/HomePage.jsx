e5c6e2b5a204bce74940738b7d9c7b2a5bc0b57f
NikoNikoUA <128256922+NikoNikoUA@users.noreply.github.com>
2024-08-14 23:15:26 +0300

    added one more component to the home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,13 @@
 import { AboutUs } from "../components/HomePage/AboutUs/AboutUs";
 import { ContactUs } from "../components/HomePage/ContactUs/ContactUs";
 import { OurWorks } from "../components/HomePage/OurWorks/OurWorks";
+import { WhatWeDo } from "../components/HomePage/WhatWeDo/WhatWeDo";
 
 const HomePage = () => {
   return (
     <>
       <AboutUs />
+      <WhatWeDo />
       <OurWorks />
       <ContactUs />
     </>
