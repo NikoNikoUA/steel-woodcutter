95644e0f95a735359d04648f7d0eab5b5e28a73e
NikoNikoUA <128256922+NikoNikoUA@users.noreply.github.com>
2024-08-15 23:13:22 +0300

    About us improved

---
+++ CommonStyles.styled.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  margin-left: auto;
+  margin-right: auto;
+
+  padding: 20px 10px;
+  max-width: 1440px;
+
+  @media screen and (min-width: 768px) {
+    padding: 0 128px;
+  }
+`;
---
+++ src/components/HomePage/AboutUs/AboutUs.styled.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Img = styled.img`
+  border-radius: 20px;
+`;
+
+export const Section = styled.section`
+  background-color: rgb(254, 255, 227);
+  padding: 50px 0;
+`;
