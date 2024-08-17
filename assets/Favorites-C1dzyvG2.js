8149743444b11ffbed1796c11defa8c16079423a
NikoNikoUA <NikoNikoUA@users.noreply.github.com>
2024-08-17 19:16:03 +0000

    deploy: bc8db9172a16e77220fa300e86c33667a7442846

--- assets/Favorites-C1dzyvG2.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-ly3UOu02.js";const s=()=>r.jsx("div",{children:"Favorites"});export{s as default};
---
+++ assets/HomePage-48cXPLyk.js
@@ -0,0 +1,91 @@
+import{d as h,j as i,R as b,r as v,l as q}from"./index-ChvtmRBq.js";const I=h.img`
+  border-radius: 20px;
+`,G=h.section`
+  background-color: rgb(254, 255, 227);
+  padding: 50px 0;
+`,F=h.div`
+  margin-left: auto;
+  margin-right: auto;
+
+  padding: 20px 10px;
+  max-width: 1440px;
+
+  @media screen and (min-width: 768px) {
+    padding: 0 128px;
+  }
+`,Z="/steel-woodcutter/assets/resized-Bf4qycP-.jpg",J="/steel-woodcutter/assets/resized_Nadia-COy96iO1.jpg",Q="/steel-woodcutter/assets/resized_Valentyn-BtbS8RqN.jpg",tt=()=>i.jsx(G,{children:i.jsxs(F,{children:[i.jsx("h1",{children:"Who we are"}),i.jsx("p",{children:"Сталевий лісоруб - це проєкт, що має за мету зробити Ваше життя яскравішим, прикрасивши його стильними і красивими виробами з дерева і металу на різноманітну тематику. Та перш за все, Сталевий Лісоруб - це люди. Тож, давайте знайомитися!"}),i.jsxs("div",{style:{display:"flex",gap:"15px",alignItems:"center",marginBottom:"30px"},children:[i.jsx(I,{alt:"image of a project creator",src:Z,width:"250",height:"360"}),i.jsxs("p",{children:[i.jsx("span",{children:"Засновник проєкту: Косинський Микола"}),i.jsx("br",{}),"Коли він почав працювати з деревом, на думку навіть не спадало, що з часом це захоплення переросте в дещо більше: в пристрасть. Бажання створюватися прості, але водночас стильні вироби, і ділитися ними з людьми - це те, що змушує Миколу рухатися вперед і не складати рук навіть у складні часи."]})]}),i.jsxs("div",{style:{display:"flex",gap:"15px",alignItems:"center",marginBottom:"30px"},children:[i.jsxs("p",{children:[i.jsx("span",{children:"Металеве серце проєкту: Косинський Валентин"}),i.jsx("br",{}),'Усі металеві конструкції, які ви зможете знайти на сайті - робота цієї людини. Майстер на усі руки - це про нього. Без цієї людини проєкт сміливо можна було б назвати просто "Лісоруб".']}),i.jsx(I,{alt:"image of Valentyn",src:Q,width:"250",height:"360"})]}),i.jsxs("div",{style:{display:"flex",gap:"15px",alignItems:"center",marginBottom:"30px"},children:[i.jsx(I,{alt:"image of Nadia",src:J,width:"250",height:"360"}),i.jsxs("p",{children:[i.jsx("span",{children:"Колір проєкту: Іванюк Надія"}),i.jsx("br",{}),"Надія додає кольорів не лише у вироби, а й в життя усієї команди. Окрім того, вона ще й активно допомагає в розробці моделей. Одним словом: незамінний член команди."]})]})]})}),et=()=>i.jsxs(i.Fragment,{children:[i.jsx("h2",{children:"Contact Us"}),i.jsx("p",{children:"Тут будуть лінки на соцмережі та контактний телефон"})]});var H={color:void 0,size:void 0,className:void 0,style:void 0,attr:void 0},R=b.createContext&&b.createContext(H),nt=["attr","size","title"];function rt(t,e){if(t==null)return{};var n=it(t,e),r,u;if(Object.getOwnPropertySymbols){var a=Object.getOwnPropertySymbols(t);for(u=0;u<a.length;u++)r=a[u],!(e.indexOf(r)>=0)&&Object.prototype.propertyIsEnumerable.call(t,r)&&(n[r]=t[r])}return n}function it(t,e){if(t==null)return{};var n={};for(var r in t)if(Object.prototype.hasOwnProperty.call(t,r)){if(e.indexOf(r)>=0)continue;n[r]=t[r]}return n}function M(){return M=Object.assign?Object.assign.bind():function(t){for(var e=1;e<arguments.length;e++){var n=arguments[e];for(var r in n)Object.prototype.hasOwnProperty.call(n,r)&&(t[r]=n[r])}return t},M.apply(this,arguments)}function W(t,e){var n=Object.keys(t);if(Object.getOwnPropertySymbols){var r=Object.getOwnPropertySymbols(t);e&&(r=r.filter(function(u){return Object.getOwnPropertyDescriptor(t,u).enumerable})),n.push.apply(n,r)}return n}function T(t){for(var e=1;e<arguments.length;e++){var n=arguments[e]!=null?arguments[e]:{};e%2?W(Object(n),!0).forEach(function(r){st(t,r,n[r])}):Object.getOwnPropertyDescriptors?Object.defineProperties(t,Object.getOwnPropertyDescriptors(n)):W(Object(n)).forEach(function(r){Object.defineProperty(t,r,Object.getOwnPropertyDescriptor(n,r))})}return t}function st(t,e,n){return e=ot(e),e in t?Object.defineProperty(t,e,{value:n,enumerable:!0,configurable:!0,writable:!0}):t[e]=n,t}function ot(t){var e=ct(t,"string");return typeof e=="symbol"?e:e+""}function ct(t,e){if(typeof t!="object"||!t)return t;var n=t[Symbol.toPrimitive];if(n!==void 0){var r=n.call(t,e||"default");if(typeof r!="object")return r;throw new TypeError("@@toPrimitive must return a primitive value.")}return(e==="string"?String:Number)(t)}function K(t){return t&&t.map((e,n)=>b.createElement(e.tag,T({key:n},e.attr),K(e.child)))}function V(t){return e=>b.createElement(at,M({attr:T({},t.attr)},e),K(t.child))}function at(t){var e=n=>{var{attr:r,size:u,title:a}=t,d=rt(t,nt),m=u||n.size||"1em",f;return n.className&&(f=n.className),t.className&&(f=(f?f+" ":"")+t.className),b.createElement("svg",M({stroke:"currentColor",fill:"currentColor",strokeWidth:"0"},n.attr,r,d,{className:f,style:T(T({color:t.color||n.color},n.style),t.style),height:m,width:m,xmlns:"http://www.w3.org/2000/svg"}),a&&b.createElement("title",null,a),t.children)};return R!==void 0?b.createElement(R.Consumer,null,n=>e(n)):e(H)}function lt(t){return V({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{fill:"none",d:"M0 0h24v24H0V0z"},child:[]},{tag:"path",attr:{d:"M15.41 16.59 10.83 12l4.58-4.59L14 6l-6 6 6 6 1.41-1.41z"},child:[]}]})(t)}function ut(t){return V({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{fill:"none",d:"M0 0h24v24H0V0z"},child:[]},{tag:"path",attr:{d:"M8.59 16.59 13.17 12 8.59 7.41 10 6l6 6-6 6-1.41-1.41z"},child:[]}]})(t)}const dt="/steel-woodcutter/assets/img_1-CRVHnVan.jpg",gt="/steel-woodcutter/assets/img_2-CUr2BLtu.jpg",ht="/steel-woodcutter/assets/img_3-b50hPMmP.jpg",ft="/steel-woodcutter/assets/img_4-5of3clJZ.jpg",pt="/steel-woodcutter/assets/img_5-uxUu1SKK.jpg",mt="/steel-woodcutter/assets/img_6-BiZI-dg2.jpg",jt="/steel-woodcutter/assets/img_7-CojO0Wkx.jpg",bt="/steel-woodcutter/assets/img_8-BpaFt1XL.jpg",xt="Left",Ot="Right",wt="Up",vt="Down",x={delta:10,preventScrollOnSwipe:!1,rotationAngle:0,trackMouse:!1,trackTouch:!0,swipeDuration:1/0,touchEventOptions:{passive:!0}},C={first:!0,initial:[0,0],start:0,swiping:!1,xy:[0,0]},B="mousemove",z="mouseup",yt="touchend",St="touchmove",Pt="touchstart";function Et(t,e,n,r){return t>e?n>0?Ot:xt:r>0?vt:wt}function A(t,e){if(e===0)return t;const n=Math.PI/180*e,r=t[0]*Math.cos(n)+t[1]*Math.sin(n),u=t[1]*Math.cos(n)-t[0]*Math.sin(n);return[r,u]}function Mt(t,e){const n=s=>{const o="touches"in s;o&&s.touches.length>1||t((c,l)=>{l.trackMouse&&!o&&(document.addEventListener(B,r),document.addEventListener(z,d));const{clientX:g,clientY:j}=o?s.touches[0]:s,p=A([g,j],l.rotationAngle);return l.onTouchStartOrOnMouseDown&&l.onTouchStartOrOnMouseDown({event:s}),Object.assign(Object.assign(Object.assign({},c),C),{initial:p.slice(),xy:p,start:s.timeStamp||0})})},r=s=>{t((o,c)=>{const l="touches"in s;if(l&&s.touches.length>1)return o;if(s.timeStamp-o.start>c.swipeDuration)return o.swiping?Object.assign(Object.assign({},o),{swiping:!1}):o;const{clientX:g,clientY:j}=l?s.touches[0]:s,[p,D]=A([g,j],c.rotationAngle),y=p-o.xy[0],S=D-o.xy[1],O=Math.abs(y),w=Math.abs(S),U=(s.timeStamp||0)-o.start,X=Math.sqrt(O*O+w*w)/(U||1),Y=[y/(U||1),S/(U||1)],k=Et(O,w,y,S),L=typeof c.delta=="number"?c.delta:c.delta[k.toLowerCase()]||x.delta;if(O<L&&w<L&&!o.swiping)return o;const P={absX:O,absY:w,deltaX:y,deltaY:S,dir:k,event:s,first:o.first,initial:o.initial,velocity:X,vxvy:Y};P.first&&c.onSwipeStart&&c.onSwipeStart(P),c.onSwiping&&c.onSwiping(P);let N=!1;return(c.onSwiping||c.onSwiped||c[`onSwiped${k}`])&&(N=!0),N&&c.preventScrollOnSwipe&&c.trackTouch&&s.cancelable&&s.preventDefault(),Object.assign(Object.assign({},o),{first:!1,eventData:P,swiping:!0})})},u=s=>{t((o,c)=>{let l;if(o.swiping&&o.eventData){if(s.timeStamp-o.start<c.swipeDuration){l=Object.assign(Object.assign({},o.eventData),{event:s}),c.onSwiped&&c.onSwiped(l);const g=c[`onSwiped${l.dir}`];g&&g(l)}}else c.onTap&&c.onTap({event:s});return c.onTouchEndOrOnMouseUp&&c.onTouchEndOrOnMouseUp({event:s}),Object.assign(Object.assign(Object.assign({},o),C),{eventData:l})})},a=()=>{document.removeEventListener(B,r),document.removeEventListener(z,d)},d=s=>{a(),u(s)},m=(s,o)=>{let c=()=>{};if(s&&s.addEventListener){const l=Object.assign(Object.assign({},x.touchEventOptions),o.touchEventOptions),g=[[Pt,n,l],[St,r,Object.assign(Object.assign({},l),o.preventScrollOnSwipe?{passive:!1}:{})],[yt,u,l]];g.forEach(([j,p,D])=>s.addEventListener(j,p,D)),c=()=>g.forEach(([j,p])=>s.removeEventListener(j,p))}return c},_={ref:s=>{s!==null&&t((o,c)=>{if(o.el===s)return o;const l={};return o.el&&o.el!==s&&o.cleanUpTouch&&(o.cleanUpTouch(),l.cleanUpTouch=void 0),c.trackTouch&&s&&(l.cleanUpTouch=m(s,c)),Object.assign(Object.assign(Object.assign({},o),{el:s}),l)})}};return e.trackMouse&&(_.onMouseDown=n),[_,m]}function Tt(t,e,n,r){return!e.trackTouch||!t.el?(t.cleanUpTouch&&t.cleanUpTouch(),Object.assign(Object.assign({},t),{cleanUpTouch:void 0})):t.cleanUpTouch?e.preventScrollOnSwipe!==n.preventScrollOnSwipe||e.touchEventOptions.passive!==n.touchEventOptions.passive?(t.cleanUpTouch(),Object.assign(Object.assign({},t),{cleanUpTouch:r(t.el,e)})):t:Object.assign(Object.assign({},t),{cleanUpTouch:r(t.el,e)})}function Dt(t){const{trackMouse:e}=t,n=v.useRef(Object.assign({},C)),r=v.useRef(Object.assign({},x)),u=v.useRef(Object.assign({},r.current));u.current=Object.assign({},r.current),r.current=Object.assign(Object.assign({},x),t);let a;for(a in x)r.current[a]===void 0&&(r.current[a]=x[a]);const[d,m]=v.useMemo(()=>Mt(f=>n.current=f(n.current,r.current),{trackMouse:e}),[e]);return n.current=Tt(n.current,r.current,u.current,m),d}const Ut=h.div`
+  height: 100%;
+  width: 100%;
+  max-width: 1100px;
+  margin: 0 auto;
+  height: 600px;
+  position: relative;
+`,kt=h.img`
+  object-fit: cover;
+  width: 100%;
+  height: 100%;
+  display: block;
+  flex-shrink: 0;
+  flex-grow: 0;
+  transition: translate 200ms ease-in-out;
+`,$=h.button`
+  all: unset;
+  display: block;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  padding: 37px;
+  cursor: pointer;
+  transition: background-color 200ms ease-in-out;
+
+  &:hover {
+    background-color: rgb(0, 0, 0, 0.5);
+  }
+
+  svg {
+    fill: black;
+    width: 30px;
+    height: 30px;
+  }
+
+  &:hover svg {
+    animation: bounce 200ms ease-in-out;
+  }
+
+  @keyframes bounce {
+    50% {
+      scale: 1.4 0.6;
+    }
+  }
+`,It=h.div`
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  display: flex;
+  gap: 5px;
+`,Ct=h.div`
+  position: absolute;
+  bottom: 10px;
+  left: 50%;
+  translate: -50%;
+  display: flex;
+  gap: 7px;
+`,_t=h.button`
+  all: unset;
+  height: 20px;
+  width: 20px;
+  background: black;
+  border: 2px solid white;
+  border-radius: 50%;
+  cursor: pointer;
+  transition: scale 100ms ease-in-out;
+
+  &:focus,
+  &:hover {
+    transform: scale(1.2);
+  }
+
+  ${t=>t.$active&&q`
+      transform: scale(1.2);
+    `}
+`,E=[dt,gt,ht,ft,pt,mt,jt,bt],Lt=()=>{const[t,e]=v.useState(0),n=()=>{e(a=>a===0?E.length-1:a-1)},r=()=>{e(a=>a===E.length-1?0:a+1)},u=Dt({onSwipedLeft:r,onSwipedRight:n,preventDefaultTouchmoveEvent:!0,trackMouse:!0});return i.jsx(i.Fragment,{children:i.jsxs(Ut,{...u,children:[i.jsx(It,{children:E.map(a=>i.jsx(kt,{src:a,style:{translate:`${-100*t}%`}},a))}),i.jsx($,{onClick:n,style:{left:"0"},children:i.jsx(lt,{})}),i.jsx($,{onClick:r,style:{right:"0"},children:i.jsx(ut,{})}),i.jsx(Ct,{children:E.map((a,d)=>i.jsx(_t,{$active:d===t,onClick:()=>e(d)},d))})]})})},Nt=()=>i.jsxs(F,{children:[i.jsx("h2",{children:"Our Works"}),i.jsx(Lt,{})]}),Rt=()=>i.jsxs(i.Fragment,{children:[i.jsx("h2",{children:"What We Do"}),i.jsx("p",{children:"Сталевий лісоруб створює красу доступну кожному. Наші штуки це не тільки прикраси, а й іграшки."})]}),Bt=()=>i.jsxs(i.Fragment,{children:[i.jsx(tt,{}),i.jsx(Rt,{}),i.jsx(Nt,{}),i.jsx(et,{})]});export{Bt as default};
