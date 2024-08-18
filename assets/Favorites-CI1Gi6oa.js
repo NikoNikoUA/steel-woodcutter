3dec42a58170dacd0a5389b8fb9e5309906506de
NikoNikoUA <NikoNikoUA@users.noreply.github.com>
2024-08-18 20:15:06 +0000

    deploy: 22b89094b00c02449541dfa9c3d3834bee577f5b

---
+++ assets/Favorites-CI1Gi6oa.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-BuzLt5tZ.js";const s=()=>r.jsx("div",{children:"Favorites"});export{s as default};
--- assets/Favorites-CIttp8TX.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-ChvtmRBq.js";const s=()=>r.jsx("div",{children:"Favorites"});export{s as default};
