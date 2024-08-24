e136883cd607acd50a18b2090f86639a252473e7
NikoNikoUA <NikoNikoUA@users.noreply.github.com>
2024-08-24 20:55:40 +0000

    deploy: a61b2acda2d78afa0efb563f9d3a51191789c5a5

--- assets/Favorites-Bpc4N8Hn.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-DHuuWY18.js";const s=()=>r.jsx("div",{children:"Favorites"});export{s as default};
--- assets/NotFound-ZlHbEU3i.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-DHuuWY18.js";const r=()=>o.jsx("div",{children:"NotFound"});export{r as default};
