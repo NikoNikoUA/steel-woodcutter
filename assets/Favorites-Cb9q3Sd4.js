5c02dd6f0357f4e985d0682e41334f4dd95d5fdb
NikoNikoUA <NikoNikoUA@users.noreply.github.com>
2024-08-25 11:18:58 +0000

    deploy: 702942047d7faca86c28ed0395038e2b85314cf5

---
+++ assets/Favorites-Cb9q3Sd4.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-D326dQv6.js";const s=()=>r.jsx("div",{children:"Favorites"});export{s as default};
---
+++ assets/NotFound-Br8iawed.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-D326dQv6.js";const r=()=>o.jsx("div",{children:"NotFound"});export{r as default};
