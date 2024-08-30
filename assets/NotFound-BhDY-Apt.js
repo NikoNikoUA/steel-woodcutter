71dd6af963cc9f778281c8d2ae20e204ebd77de1
NikoNikoUA <NikoNikoUA@users.noreply.github.com>
2024-08-30 19:14:20 +0000

    deploy: fae3ab21900200c8673767ebd8da7769399ef0e8

---
+++ assets/NotFound-BhDY-Apt.js
@@ -0,0 +1,1 @@
+import{j as o,S as t,C as n}from"./index-C_Gp07FK.js";const s=()=>o.jsx(t,{children:o.jsx(n,{children:"NotFound"})});export{s as default};
--- assets/NotFound-CI8alg8L.js
+++
@@ -1,1 +0,0 @@
-import{j as o,S as t,C as n}from"./index-CHVIeCee.js";const s=()=>o.jsx(t,{children:o.jsx(n,{children:"NotFound"})});export{s as default};
